502a640bd33e6d29aa1437d091e4f03626607b1d
VicenteVicente <vecalisto@uc.cl>
2020-08-21 19:09:31 -0400

    Updates

---
+++ precache-manifest.d66d6a2c9851c29ceb78ba3f13068780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a342a34e05b3b00f888b465c6ee677af",
+    "url": "/REmatch/index.html"
+  },
+  {
+    "revision": "93b93c993a11735acedc",
+    "url": "/REmatch/static/css/2.be193143.chunk.css"
+  },
+  {
+    "revision": "8d8455977a65985cf112",
+    "url": "/REmatch/static/css/main.e22fa949.chunk.css"
+  },
+  {
+    "revision": "93b93c993a11735acedc",
+    "url": "/REmatch/static/js/2.2ba9fe58.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/REmatch/static/js/2.2ba9fe58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8455977a65985cf112",
+    "url": "/REmatch/static/js/main.5746ac58.chunk.js"
+  },
+  {
+    "revision": "e2d48d7e01a056ce9698",
+    "url": "/REmatch/static/js/runtime-main.2a4defc6.js"
+  }
+]);
