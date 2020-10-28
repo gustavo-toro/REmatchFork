dcab8071f6bd6fb8bd4fe5943049ac13df8b9a42
nicovsj <nicovsj@uc.cl>
2020-10-28 02:27:57 -0300

    added swig win support

--- REmatchEngine/tests/swig/simple-test.py
+++ REmatchEngine/tests/swig/simple-test.py
@@ -2,7 +2,7 @@ import os.path
 import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.abspath(dir_path + '/' + '../../build/Release/bin/SWIG'))
+sys.path.append(os.path.abspath(dir_path + '/' + '../../build/bin/SWIG'))
 
 import rematch as re
 
@@ -14,7 +14,6 @@ match = rgx.findIter(document)
 
 while match:
   for var in match.variables():
-    print("!{}:".format(var), match.span("x"), end='\t')
+    print("!{}:".format(var), match.span(var), end='\t')
   print("")
-  print(match.doc())
   match = rgx.findIter(document)
