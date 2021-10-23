b7734d7a0dff5f22ec53823fd3ef3a9cbf7512ef
VicenteVicente <vecalisto@uc.cl>
2021-10-23 16:32:43 -0300

    feat: Match::spans

--- src/rematch/match.cpp
+++ src/rematch/match.cpp
@@ -8,6 +8,22 @@ Match::operator bool() const {
   return false;
 };
 
+std::vector<std::string> Match::variables() const {
+  return var_factory_->variables();
+};
+
+SpanVec Match::spans(std::string var) const {
+  try {
+    SpanVec ret;
+    int pos = var_factory_->position(var);
+    for (size_t i = 0; i < data_[pos].size() / 2; ++i)
+      ret.push_back(std::make_pair(data_[pos][2 * i], data_[pos][2 * i + 1]));
+    return ret;
+  } catch (...) {
+    throw std::logic_error("No mapping assigned to variable.");
+  }
+};
+
 std::string Match::pprint(std::shared_ptr<StrDocument>& doc) const {
   std::stringstream ss;
   ss << "not implemented.";
--- src/rematch/match.hpp
+++ src/rematch/match.hpp
@@ -18,6 +18,12 @@ class StrDocument;
   using Match_ptr = std::unique_ptr<Match>;
 #endif
 
+using Span = std::pair<int64_t,int64_t>;
+using SpanVec = std::vector<Span>;
+
+// Represents a match for easy access to the captured spans and substrings.
+// It doesn't store the correspondings substrings, so it's assumed that the
+// sublaying document is available.
 class Match {
   friend class Enumerator;
   friend class Interface;
@@ -25,12 +31,19 @@ class Match {
  public:
 
   Match() = default;
+
   // Match is true if at least ONE variable captured spans
   operator bool() const;
+
   // Returns a vector with the variable names in order
-  // std::vector<std::string> variables() const;
+  std::vector<std::string> variables() const;
+
+  // Returns a vector of pairs mapped to var
+  SpanVec spans(std::string var) const;
+
   // Pretty print Match
   std::string pprint(std::shared_ptr<StrDocument>& doc) const;
+
   // Output format of matches
   friend std::ostream& operator<<(std::ostream &os, Match &m);
 
@@ -38,8 +51,10 @@ class Match {
   // Only Enumerator is able to construct a Match
   Match(std::shared_ptr<VariableFactory> vf, std::vector<std::deque<int64_t>> m)
       : var_factory_(vf), data_(m) {}
+
   // Access to variable names
   std::shared_ptr<VariableFactory> var_factory_;
+
   // Match main data
   std::vector<std::deque<int64_t>> data_;
 };
