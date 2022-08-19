4bb943f5eff91873fe42046ddf2adbc6857d4338
VicenteVicente <vecalisto@uc.cl>
2022-08-18 22:47:22 -0400

    feat: variable factory visitor

--- src/rematch/parse/parser.cpp
+++ src/rematch/parse/parser.cpp
@@ -2,23 +2,29 @@
 
 namespace rematch {
 
-void doParse(const std::string& input) {
+void doParse(const std::string &input) {
   std::cout << "Input: " << input << std::endl;
-  
-  // Build parse tree
+
+  // * Parse tree
   antlr4::ANTLRInputStream stream(input);
   REmatchLexer lexer(&stream);
   antlr4::CommonTokenStream tokens(&lexer);
   REmatchParser parser(&tokens);
-  REmatchParser::RootContext* root = parser.root();
+  REmatchParser::RootContext *root = parser.root();
   std::cout << "Parse Tree Built." << std::endl;
+
+  // * Variable Factory
+  visitors::VariableFactoryVisitor vfv;
+  VariableFactory vfact = vfv.create_vfact(root);
+  std::cout << "Variable Factory Built." << std::endl;
+  std::cout << vfact.pprint() << std::endl;
 }
 
 std::unique_ptr<LogicalVA> regex2LVA(std::string regex) {
   doParse(regex);
   std::cout << "FINISHED!" << std::endl;
 
-	return nullptr;
+  return nullptr;
 }
 
 } // end namespace rematch
--- src/rematch/parse/visitors.hpp
+++ src/rematch/parse/visitors.hpp
@@ -1,9 +1,10 @@
 #ifndef PARSER__VISITORS_HPP
 #define PARSER__VISITORS_HPP
 
-#include "parse/visitors/filter_factory_visitor.hpp"
-#include "parse/visitors/automata_visitor.hpp"
-#include "parse/visitors/variable_factory_visitor.hpp"
-#include "parse/visitors/epsilon_visitor.hpp"
+// #include "parse/visitors/automata_visitor.hpp"
+// #include "parse/visitors/epsilon_visitor.hpp"
+// #include "parse/visitors/filter_factory_visitor.hpp"
+// #include "parse/visitors/variable_factory_visitor.hpp"
+#include "parse/visitors/antlr/variable_factory_visitor.hpp"
 
 #endif
